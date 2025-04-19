53c77e6db9f127fdf1e5da651a937a0d5306a93f
Ruslan <randomrocketgalaxy@gmail.com>
2025-04-19 16:43:52 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,211 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import usersJson from './data/users.json';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
+import { Button } from './components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './components/ui/dialog';
+import { Input } from './components/ui/input';
+import { useForm } from 'react-hook-form'; 
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod'; 
+import Navbar from './components/ui/Navbar';
+
+interface user{
+  id?: string;
+  name?: string;
+  email?: string;
+  role?: string;
+}
+
+const formSchema = z.object({
+  id: z.string().min(2, {
+    message: "Name must have at least 2 characters"
+  }),
+  name: z.string().min(2, {
+    message: "Name must have at least 2 characters"
+  }),
+  email: z.string().email({
+    message: "Enter a valid email address"
+  }),
+  role: z.string().min(2, {
+    message: "Role must have at least 2 characters"
+  }),
+})
+
+
+function App() {
+  const [newUser, setNewUser] = useState<user>();
+  const [existingUser, setExistingUser] = useState<user>();
+  const [users, setUsers] = useState<user[]>(usersJson);
+  
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      id:"123",
+      name:"",
+      email:"",
+      role:""
+    }
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Process form data
+    console.log(values)
+
+    // Close the dialog after successful submission
+    // setOpen(false)
+
+    // Reset the form
+    form.reset()
+  }
+  let dummyUser:user = {id:"2",name:"2",email:"1",role:"moderator"} 
+
+  useEffect(()=>{
+    localStorage.setItem("users",JSON.stringify(users));
+  },[users]);
+
+  function updateUser(){
+    let tempUsers = users;
+    let index = tempUsers.findIndex(user => user.id == existingUser?.id);
+    tempUsers[index].id = existingUser?.id;
+    tempUsers[index].name = existingUser?.name;
+    tempUsers[index].email = existingUser?.email;
+    tempUsers[index].role = existingUser?.role;
+    setUsers([...tempUsers]);
+  }
+
+  function addNewUser(){
+    let newId = crypto.randomUUID();
+    setUsers([...users,{...newUser, id: newId}]);
+    setNewUser({id:"",name:"",email:"",role:""});
+  }
+
+
+
+  return(
+    <>
+    <Navbar/>
+
+    <div className='flex flex-col px-9'>
+      <div className='flex flex-row'>
+        <Dialog>
+          <DialogTrigger>
+            <Button>New User</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add New Profile</DialogTitle>
+              <DialogDescription>
+                Add in the details of the new profile. Click submit when you're done.
+              </DialogDescription>
+            </DialogHeader>
+              <form className="grid gap-4 py-4" onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <label htmlFor="Name" className="text-right">
+                    Name
+                  </label>
+                  <Input id="Name" value={newUser?.name} className="col-span-3" onChange={e=>{setNewUser({...newUser, name:e.target.value})}}/>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <label htmlFor="Email" className="text-right">
+                    Email
+                  </label>
+                  <Input id="Email" value={newUser?.email} className="col-span-3"  onChange={e=>{setNewUser({...newUser, email:e.target.value})}}/>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <label htmlFor="Role" className="text-right">
+                    Role
+                  </label>
+                  <Input id="Role" value={newUser?.role} className="col-span-3"  onChange={e=>{setNewUser({...newUser, role:e.target.value})}}/>
+                </div>
+              </form>
+            <DialogFooter>
+              <Button type='submit' onClick={()=>addNewUser()}>Submit</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+
+      <Table>
+        <TableCaption>My table</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow key={users[1].id}>
+              <TableCell>{users[1].id}</TableCell>
+              <TableCell>{users[1].name}</TableCell>
+              <TableCell>{users[1].email}</TableCell>
+              <TableCell>{users[1].role}</TableCell>
+              <TableCell><img src="src/assets/icons/dotsIcon.png" alt="Actions" className='size-[20px]'/>
+              <div className='flex flex-col absolute bottom-[-15] right-0 z-10 rounded-[3px] bg-[#f0f0f0] p-2'>
+                <Dialog>
+                  <DialogTrigger>
+                    <Button variant="secondary" size="sm" onClick={()=>{setExistingUser(users[1])}}>Edit</Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Edit Profile</DialogTitle>
+                      <DialogDescription>
+                        Make changes to the profile here. Click submit when you're done.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <div className="grid grid-cols-4 items-center gap-4">
+                        <label htmlFor="Name" className="text-right">
+                          Name
+                        </label>
+                        <Input id="Name" value={existingUser?.name} className="col-span-3" onChange={e=>{setExistingUser({...existingUser, name:e.target.value})}}/>
+                      </div>
+                      <div className="grid grid-cols-4 items-center gap-4">
+                        <label htmlFor="Email" className="text-right">
+                          Email
+                        </label>
+                        <Input id="Email" value={existingUser?.email} className="col-span-3"  onChange={e=>{setExistingUser({...existingUser, email:e.target.value})}}/>
+                      </div>
+                      <div className="grid grid-cols-4 items-center gap-4">
+                        <label htmlFor="Role" className="text-right">
+                          Role
+                        </label>
+                        <Input id="Role" value={existingUser?.role} className="col-span-3"  onChange={e=>{setExistingUser({...existingUser, role:e.target.value})}}/>
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <Button type='submit' onClick={()=>updateUser()}>Submit Changes</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+                <Button variant="destructive" size="sm">Delete</Button>
+              </div>
+              </TableCell>
+          </TableRow>
+          {users.map(user=>{
+            return(<TableRow key={user.id}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.role}</TableCell>
+              <TableCell>
+                <Button variant="ghost" size="icon">
+                  <img src="src/assets/icons/dotsIcon.png" alt="Actions" className='size-[20px] cursor-pointer'/>
+                </Button>
+              {/* <div className='flex flex-col absolute bottom-[-15] right-0 z-10 rounded-sm bg-[#f0f0f0] p-1'>
+                <Button variant="secondary">Edit</Button>
+                <Button variant="destructive">Delete</Button>
+              </div> */}
+              </TableCell>
+            </TableRow>);
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  </>
+  );
+}
+
+export default App
---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,14 @@
+
+export default function Navbar(){
+    return(
+        <nav className='flex flex-row justify-between bg-[#1C1A27] pt-4 pb-4 w-[100%] mb-[3rem] px-[3rem] border-white'>
+            <div className='flex flex-row align-bottom self-center'>
+                <p className='align-baseline text-[2rem] self-center font-sg'>ConnectSphere Community</p>
+            </div>
+            <div className='flex flex-row gap-3 justify-items-end'>
+                <img src="src/assets/icons/UserIcon.png" alt="profile" className='self-center size-8' />
+                <img src="src/assets/icons/LogOutIcon.png" alt="log-out" className='self-center size-8'/>
+            </div>
+        </nav>
+    );
+}
